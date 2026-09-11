2dfa01cf6b1f58c6204043a49ebc61847df1069e
Daruma <haruto.0913.0105@gmail.com>
2026-09-11 21:03:39 +0900

    release: bump toolchain installer channel to 0.0.7-beta

--- Runtime/Tooling/Providers/Installer/toolchain_installer.py
+++ Runtime/Tooling/Providers/Installer/toolchain_installer.py
@@ -15,7 +15,7 @@ from Runtime.Tooling.Providers.Installer.release_installer import (
     install_plan as install_release_plan,
 )
 
-CHANNEL = "0.0.6-beta"
+CHANNEL = "0.0.7-beta"
 
 
 def _now() -> str:
