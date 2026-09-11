9f76b7e3c351464e04284f996b926e2cac953683
Daruma <haruto.0913.0105@gmail.com>
2026-09-11 17:53:53 +0900

    chore: bump layer validator canonical version

--- Tools/LayerBoundaryValidator/validate_layer_boundaries.py
+++ Tools/LayerBoundaryValidator/validate_layer_boundaries.py
@@ -28,7 +28,7 @@ EXPECTED_EDGES = {
     ("provider_layer", "evidence_state", "structured_result_capture"),
 }
 FORBIDDEN_ENTRY_TEXT = ("unity artist", "unity-artist")
-CANONICAL_VERSION = "0.0.3-beta"
+CANONICAL_VERSION = "0.0.4-beta"
 _GUID_PATTERN = re.compile(r"^guid:\s*([0-9a-f]{32})$", re.MULTILINE)
 
 
