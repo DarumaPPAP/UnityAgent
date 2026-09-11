d5c7af89b0bf424e169d00f438a894510e854b40
Daruma <haruto.0913.0105@gmail.com>
2026-09-11 19:18:06 +0900

    release: bump Installer Provider channel to 0.0.6-beta

--- Runtime/Tooling/Providers/Installer/installer_provider.py
+++ Runtime/Tooling/Providers/Installer/installer_provider.py
@@ -30,7 +30,7 @@ PRODUCTS = frozenset({
     "unity_agent_codex_plugin",
 })
 PACKAGE_IDS = ("com.unity-artist", "com.darumappap.unity-artist")
-CHANNEL = "0.0.5-beta"
+CHANNEL = "0.0.6-beta"
 
 
 def _now() -> str:
